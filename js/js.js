a73d3c7157a5814d47615fa2adea1c9c40eb777f
zenua <zenuaa@outlook.com>
2021-07-08 01:13:10 +0300

    Make timer sale

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
-
+    console.time('time');
+    
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('div.tabcontent'),
         tabsParent = tabs[0].parentNode;
@@ -25,7 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
     tabsParent.addEventListener('click', (event) => {
         if (event.target && event.target.classList.contains('tabheader__item')) {
             tabs.forEach((item, index) => {
-                if (item == event.target ) {
+                if (item == event.target) {
                     hideContent();
                     showContent(index);
                 }
@@ -33,4 +34,56 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     });
+
+
+    // date
+
+    const doomsDay = '2022-02-17';
+
+    const upTime = setInterval(setLostTime, 1000);
+    setLostTime();
+    
+    function getLostTime(doomsDaySet) {
+        const currentTime = new Date().getTime(),
+            difference = new Date(doomsDay).getTime() - currentTime,
+            day = Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(difference / (1000 * 60 * 60) % 24),
+            min = Math.floor(difference / (1000 * 60) % 60),
+            sec = Math.floor(difference / (1000) % 60);
+            return {
+                day: day,
+                hours: hours,
+                min: min,
+                sec: sec,
+                diff: difference
+            };
+    }
+
+    function setLostTime() {
+        const objLostTime = getLostTime(doomsDay),
+        timerBlock = document.querySelectorAll('.timer__block');
+        let arrLostTime = Object.values(objLostTime);
+        if (objLostTime.diff <= 0) {
+            clearInterval(upTime);
+        } else {
+            timerBlock.forEach((item, index) => {
+
+                 let value = arrLostTime[index];
+                 if(value < 10){
+                    item.firstElementChild.textContent = '0' + value;
+                 }
+                else {
+                    item.firstElementChild.textContent = value;
+                }
+            });
+        }
+    }
+
+   
+    //end date
+
+
+    console.timeEnd('time');
+    // end DOMContentLoaded
 });
+
